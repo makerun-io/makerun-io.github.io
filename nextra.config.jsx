ee8a41cc808ef80eb7bd142a7fda2d6c784f4904
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2024-02-23 18:10:00 +0100

    add  a favicon.ico

--- nextra.config.jsx
+++ nextra.config.jsx
@@ -3,6 +3,18 @@ export default {
   project: {
     link: 'https://github.com/makerun-io/makerun-io.github.io'
   },
-  docsRepositoryBase: 'https://github.com/makerun-io/makerun-io.github.io'
+  docsRepositoryBase: 'https://github.com/makerun-io/makerun-io.github.io/tree/main',
+  footer: {
+    text: (
+      <span>
+        {new Date().getFullYear()} ©{' '}
+        <a href="https://www.makerun.io">
+          makerun.io
+        </a>
+        .
+      </span>
+    )
+  }
   // ... other theme options
+
 }
